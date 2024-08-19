d355d4d8dd956b61cb53507f955265dbd6e717d9
tidvn <tiendung0325@gmail.com>
2024-08-19 14:51:37 +0700

    fix

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,17 +1,12 @@
-import { config } from "process";
-
 /** @type {import('prettier').Config} */
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
-  // tailwindcss
+  trailingComma: "es5",
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+
   tailwindAttributes: ["theme"],
   tailwindFunctions: ["twMerge", "createTheme"],
-  config: {
-    trailingComma: "es5",
-    semi: true,
-    tabWidth: 2,
-    singleQuote: true,
-    jsxSingleQuote: true,
-    plugins: ["prettier-plugin-tailwindcss"],
-  },
 };
